491859c6cee20a765bbf06587d06f0459aee61b6
bladedevleoper <jamesjonestred@gmail.com>
2020-08-02 01:13:15 +0100

    minor update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,35 +5,54 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <div class="card-header">{{ __('Company') }}</div>
+                    <div class="card-body">
+                        <div class="container">
+                        <div class="row">
+                                <ul class="list-inline mx-auto">
+                                    @if (auth()->user()->is_admin)
+                                        <li class="list-inline-item">
+                                            <a href="{{ route('company.create') }}" class="btn btn-primary">
+                                                Add Company
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="{{ url('show-companies') }}" class="btn btn-primary">
+                                                Show Companies
+                                            </a>
+                                        </li>
+                                    @else
+                                        <li class="list-inline-item">
+                                            <a href="{{ url('show-companies') }}" class="btn btn-primary">
+                                                Show Companies
+                                            </a>
+                                        </li>
+                                    @endif
+                                </ul>
                         </div>
-                    @endif
+                    </div>
+                </div>
+            </div>
 
+            <div class="card mt-5">
+                <div class="card-header">{{ __('Employee') }}</div>
+                <div class="card-body">
                     <div class="container">
                         <div class="row">
                             <ul class="list-inline mx-auto">
                                 @if (auth()->user()->is_admin)
                                     <li class="list-inline-item">
-                                        <a href="{{ route('company.create') }}" class="btn btn-primary">
-                                           Add Company
-                                        </a>
+                                        <button type="button" class="btn btn-warning">
+                                            Add Employee
+                                        </button>
                                     </li>
                                     <li class="list-inline-item">
                                         <button type="button" class="btn btn-warning">
-                                           Add Employee
+                                            Show Employees
                                         </button>
                                     </li>
                                 @else
-                                    <li class="list-inline-item">
-                                        <a href="{{ url('show-companies') }}" class="btn btn-primary">
-                                            Show Companies
-                                        </a>
-                                    </li>
+
                                 @endif
                             </ul>
                         </div>
