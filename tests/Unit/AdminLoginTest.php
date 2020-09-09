ace6da2301e97edd8652bc16267780f23a5705fd
bladedevleoper <jamesjonestred@gmail.com>
2020-09-09 21:33:56 +0100

    update to tests for admin and non admin user

--- tests/Unit/AdminLoginTest.php
+++ tests/Unit/AdminLoginTest.php
@@ -37,6 +37,7 @@ class AdminLoginTest extends TestCase
         $this->adminUser = User::where('id', 1)->get()->first();
         $this->adminUser->is_admin = true;
 
+
         $this->nonAdminUser = User::where('id', 2)->get()->first();
 
 
@@ -52,28 +53,31 @@ class AdminLoginTest extends TestCase
     }
 
     /**
-     * this will test that an admin user can log in
+     * this will test that a user can log in
+     * and the user is redirected to home
      * @test
      */
-    public function testThatAdminCanLogin()
+    public function testThatValidUserCanLoginAndRedirectToHome()
     {
-        //todo check if the user is admin, cannot remember if I check for is_admin property
+
        $response = $this->post('/login', [
-           'email' => $this->adminUser->email,
-           'password' => 'supernova2005',
-           '_token' => csrf_token(),
+               'email' => $this->adminUser->email,
+               'password' => 'supernova2005',
+               '_token' => csrf_token(),
        ]);
 
-        $response->isRedirect('/');
-        //$response->as
-
-       //TODO use this to test that the isAdmin is coming back within the view
-       //$response->viewData('isAdmin');
-
-      $response->assertRedirect('home');
+       $response->assertRedirect('/home');
 
     }
 
+    public function testThatUserIsAdmin()
+    {
+       $response = $this->actingAs($this->adminUser)
+            ->get('/home');
+
+
+       $this->assertEquals(true, $response->viewData('isAdmin'));
+    }
     /**
      * this will test for a non admin user can login
      * @test
@@ -82,7 +86,7 @@ class AdminLoginTest extends TestCase
     {
 
         $response = $this->post('/login', [
-            'email' => $this->nonAdminUser->email,
+            'email' => $this->adminUser->email,
             'password' => 'fakePassword',
             '_token' => csrf_token(),
         ]);
@@ -95,7 +99,7 @@ class AdminLoginTest extends TestCase
     {
 
         $response = $this->post('/login', [
-            'email' => $this->nonAdminUser->email,
+            'email' => $this->adminUser->email,
             'password' => 'fakePassword',
             '_token' => csrf_token(),
         ]);
